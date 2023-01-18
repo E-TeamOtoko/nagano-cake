d931d2d19562f34bdc56e1b80ba7cfbce633d771
Kudo Rintaro <119265020+KudoRintaro@users.noreply.github.com>
2023-01-18 08:34:30 +0000

    全体レイアウト(仮)追加

--- app/controllers/item_controller.rb
+++
@@ -1,7 +0,0 @@
-class ItemController < ApplicationController
-  def index
-  end 
-  
-  def show
-  end 
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_01_18_050525) do
+ActiveRecord::Schema.define(version: 2023_01_18_062541) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -40,6 +40,27 @@ ActiveRecord::Schema.define(version: 2023_01_18_050525) do
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
   end
 
+  create_table "cart_items", force: :cascade do |t|
+    t.integer "customer_id", null: false
+    t.integer "item_id", null: false
+    t.integer "quantity", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["customer_id"], name: "index_cart_items_on_customer_id"
+    t.index ["item_id"], name: "index_cart_items_on_item_id"
+  end
+
+  create_table "items", force: :cascade do |t|
+    t.integer "genre_id", null: false
+    t.string "item_name", null: false
+    t.integer "tax_excluded_price", null: false
+    t.integer "sales_status", default: 1, null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["genre_id"], name: "index_items_on_genre_id"
+    t.index ["tax_excluded_price"], name: "index_items_on_tax_excluded_price"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -54,4 +75,7 @@ ActiveRecord::Schema.define(version: 2023_01_18_050525) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "cart_items", "customers"
+  add_foreign_key "cart_items", "items"
+  add_foreign_key "items", "genres"
 end
