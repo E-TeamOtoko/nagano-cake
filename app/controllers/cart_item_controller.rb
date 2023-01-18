a0c6cd5bf6256d4b724c28b5fbc673cc7458bd8a
Kudo Rintaro <119265020+KudoRintaro@users.noreply.github.com>
2023-01-18 06:50:52 +0000

    モデル、コントローラ追加

---
+++ app/controllers/cart_item_controller.rb
@@ -0,0 +1,16 @@
+class CartItemController < ApplicationController
+  def index
+  end 
+  
+  def create
+  end 
+  
+  def update 
+  end 
+  
+  def destroy
+  end  
+  
+  def destroy_all
+  end 
+end
