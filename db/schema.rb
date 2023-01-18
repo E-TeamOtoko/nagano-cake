ac616b66fd4fe1f80a900c26c8648938ba656d08
AaronMorimoto <119596908+AaronMorimoto@users.noreply.github.com>
2023-01-18 09:41:40 +0000

    スキーマ修正

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
-ActiveRecord::Schema.define(version: 2023_01_18_062541) do
-=======
-
-ActiveRecord::Schema.define(version: 2023_01_18_061903) do
-
->>>>>>> origin/develop
+ActiveRecord::Schema.define(version: 2023_01_18_074601) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -46,32 +40,16 @@ ActiveRecord::Schema.define(version: 2023_01_18_061903) do
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
   end
 
-<<<<<<< HEAD
-  create_table "cart_items", force: :cascade do |t|
+  create_table "addresses", force: :cascade do |t|
     t.integer "customer_id", null: false
-    t.integer "item_id", null: false
-    t.integer "quantity", null: false
+    t.string "name", null: false
+    t.string "address", null: false
+    t.string "postal_code", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.index ["customer_id"], name: "index_cart_items_on_customer_id"
-    t.index ["item_id"], name: "index_cart_items_on_item_id"
   end
 
-  create_table "items", force: :cascade do |t|
-    t.integer "genre_id", null: false
-    t.string "item_name", null: false
-    t.integer "tax_excluded_price", null: false
-    t.integer "sales_status", default: 1, null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["genre_id"], name: "index_items_on_genre_id"
-    t.index ["tax_excluded_price"], name: "index_items_on_tax_excluded_price"
-  end
-
-  create_table "users", force: :cascade do |t|
-=======
   create_table "admins", force: :cascade do |t|
->>>>>>> origin/develop
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
@@ -82,22 +60,17 @@ ActiveRecord::Schema.define(version: 2023_01_18_061903) do
     t.index ["email"], name: "index_admins_on_email", unique: true
     t.index ["reset_password_token"], name: "index_admins_on_reset_password_token", unique: true
   end
-  
-  create_table "genres", force: :cascade do |t|
-    t.string "name"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
-  
-  create_table "addresses", force: :cascade do |t|
+
+  create_table "cart_items", force: :cascade do |t|
     t.integer "customer_id", null: false
-    t.string "name", null: false
-    t.string "address", null: false
-    t.string "postal_code", null: false
+    t.integer "item_id", null: false
+    t.integer "quantity", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.index ["customer_id"], name: "index_cart_items_on_customer_id"
+    t.index ["item_id"], name: "index_cart_items_on_item_id"
   end
-  
+
   create_table "customers", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -116,6 +89,23 @@ ActiveRecord::Schema.define(version: 2023_01_18_061903) do
     t.datetime "updated_at", precision: 6, null: false
     t.index ["email"], name: "index_customers_on_email", unique: true
     t.index ["reset_password_token"], name: "index_customers_on_reset_password_token", unique: true
+  end
+
+  create_table "genres", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "items", force: :cascade do |t|
+    t.integer "genre_id", null: false
+    t.string "item_name", null: false
+    t.integer "tax_excluded_price", null: false
+    t.integer "sales_status", default: 1, null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["genre_id"], name: "index_items_on_genre_id"
+    t.index ["tax_excluded_price"], name: "index_items_on_tax_excluded_price"
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
