c5fe191cf1e1a7429de491f42127e7511f1812d5
AaronMorimoto <119596908+AaronMorimoto@users.noreply.github.com>
2023-01-19 07:12:01 +0000

    不必要

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_01_19_051143) do
+ActiveRecord::Schema.define(version: 2023_01_18_074601) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
