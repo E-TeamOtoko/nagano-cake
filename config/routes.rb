1421aae3bd7e89802474cc07cfb8640305afd82e
Kudo Rintaro <119265020+KudoRintaro@users.noreply.github.com>
2023-01-20 17:00:30 +0000

    商品新規登録編集中

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   devise_for :customers
   devise_for :admins
+
+  root to: "admin/items#new"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
