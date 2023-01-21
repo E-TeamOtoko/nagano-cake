a529c655ec8510d20ac994128a81227fe40009c0
suisen26 <119019835+suisen26@users.noreply.github.com>
2023-01-21 10:53:11 +0000

    [Initial] ジャンル管理画面

--- app/controllers/admin/genres_controller.rb
+++ app/controllers/admin/genres_controller.rb
@@ -1,13 +1,29 @@
 class Admin::GenresController < ApplicationController
   def index
+    @genre = Genre.new
+    @genres = Genre.all
   end
 
   def create
+    @genre = Genre.new(genre_params)
+    if @genre.save
+      render 'index'
+      #redirect_to request.referer
+    else
+      @genres = Genre.all
+      render 'index'
+    end
   end
 
   def edit
   end
 
   def update
+  end
+
+  private
+
+  def genre_params
+    params.require(:genre).permit(:name)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,15 @@
 Rails.application.routes.draw do
-  devise_for :customers
-  devise_for :admins
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  devise_for :customers, skip: [:passwords], controllers: {
+    registrations: "public/registrations",
+    sessions: "public/sessions"
+  }
+  
+  devise_for :admin, skip: [:registrations, :passwords], controllers: {
+    sessions: "admin/sessions"
+  }
+
+  namespace :admin do
+    resources :genres, only: [:index, :create, :edit, :update]
+  end
 end
