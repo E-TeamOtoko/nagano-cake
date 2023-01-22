ebc9948bf1496c54bacd40ca8ae3f6d2c2141eb5
Kudo Rintaro <119265020+KudoRintaro@users.noreply.github.com>
2023-01-22 15:50:12 +0000

    コントローラ、ビューファイル編集(コメントアウト中)

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -4,21 +4,27 @@ class Admin::ItemsController < ApplicationController
   end
 
   def create
-    @item=Item.new(item_params)
-    @item.save
-    redirect_to edit_item_path(@item.id)
+    #@item=Item.new(item_params)
+    #@item.save
+    #redirect_to admin_item_path(@item.id)
   end
 
   def index
+    #@items=Item.all
   end
 
   def show
+    #@item=Item.find(params[:id])
   end
 
   def edit
+    #@item=Item.find(params[:id])
   end
 
   def update
+    #@item=Item.find(params[:id])
+    #@item.update(item_params)
+    #redirect_to admin_item_path(@item.id)
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,13 @@
 Rails.application.routes.draw do
   devise_for :customers
   devise_for :admins
-
+  get "admin/items/test" => "admin/items#index"
   get "admin/items/new" => "admin/items#new"
   post "admin/items" => "admin/items#create"
-
-  scope "/admin" do
-    resources :items, only: [:index, :show, :edit, :update, :destroy]
-  end
-
+  
+  #scope "/admin" do
+    #resources :items, only[:index, :show, :edit, :update]
+  #end 
   scope "/public" do
     resources :items, only: [:index, :show]
   end
