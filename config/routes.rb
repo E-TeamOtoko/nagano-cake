fd42634502209dabb54acb5dff3cda2d73a3735a
suisen26 <119019835+suisen26@users.noreply.github.com>
2023-01-23 09:42:03 +0000

    ジャンルのnameカラムにnull:falseを追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,23 +1,6 @@
 Rails.application.routes.draw do
 
-  devise_for :customers
-  devise_for :admins
-  get "admin/items/new" => "admin/items#new"
-  post "admin/items" => "admin/items#create"
-
-  scope "/admin" do
-    resources :items, only: [:index, :show, :edit, :update]
-  end
-  scope "/public" do
-    resources :items, only: [:index, :show]
-    resources :cart_items do
-      member do
-       delete "destroy_all"
-      end
-    end
-  end
-
-     devise_for :customers, skip: [:passwords], controllers: {
+  devise_for :customers, skip: [:passwords], controllers: {
     registrations: "public/registrations",
     sessions: "public/sessions"
   }
