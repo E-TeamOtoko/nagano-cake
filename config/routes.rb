aa9c953da77a882ce62ce36bc1a7d7d7c54e67ec
Kudo Rintaro <119265020+KudoRintaro@users.noreply.github.com>
2023-01-23 09:41:11 +0000

    ルーティング修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,26 +1,10 @@
 Rails.application.routes.draw do
-
-  devise_for :customers
-  devise_for :admins
-  get "admin/items/new" => "admin/items#new"
-  post "admin/items" => "admin/items#create"
-
-  scope "/admin" do
-    resources :items, only: [:index, :show, :edit, :update]
-  end
-  scope "/public" do
-    resources :items, only: [:index, :show]
-    resources :cart_items do
-      member do
-       delete "destroy_all"
-      end
-    end
-  end
-
-     devise_for :customers, skip: [:passwords], controllers: {
+  
+  devise_for :customers, skip: [:passwords], controllers: {
     registrations: "public/registrations",
     sessions: "public/sessions"
   }
+
 
   devise_for :admin, skip: [:registrations, :passwords], controllers: {
     sessions: "admin/sessions"
@@ -28,6 +12,16 @@ Rails.application.routes.draw do
 
   namespace :admin do
     resources :genres, only: [:index, :create, :edit, :update, :destroy]
+    resources :items
+  end
+  
+  namespace :public do
+    resources :items, only: [:index, :show]
+    resources :cart_items do
+      member do
+       delete "destroy_all"
+      end
+    end
   end
 end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
