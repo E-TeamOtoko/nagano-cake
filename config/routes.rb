afdf6caa6020180a4e52d1eefefcd09b90451ec5
Kudo Rintaro <119265020+KudoRintaro@users.noreply.github.com>
2023-01-23 07:12:46 +0000

    ルーティング修正

--- config/routes.rb
+++ config/routes.rb
@@ -15,18 +15,5 @@ Rails.application.routes.draw do
       end
     end
   end
-
-   devise_for :customers, skip: [:passwords], controllers: {
-    registrations: "public/registrations",
-    sessions: "public/sessions"
-  }
-
-  devise_for :admin, skip: [:registrations, :passwords], controllers: {
-    sessions: "admin/sessions"
-  }
-
-  namespace :admin do
-    resources :genres, only: [:index, :create, :edit, :update, :destroy]
-  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
