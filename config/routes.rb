c857b052812dc4bee6ba71bef55f371094033048
Kudo Rintaro <119265020+KudoRintaro@users.noreply.github.com>
2023-01-23 07:25:45 +0000

    ルーティング修正

--- config/routes.rb
+++ config/routes.rb
@@ -29,5 +29,5 @@ Rails.application.routes.draw do
     resources :genres, only: [:index, :create, :edit, :update, :destroy]
   end
   end
-end 
+end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
