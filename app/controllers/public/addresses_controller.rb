00dd7534fc9c63475617f01d242a8cc54f059755
AaronMorimoto <119596908+AaronMorimoto@users.noreply.github.com>
2023-01-23 15:15:19 +0000

    アクション追加2

--- app/controllers/public/addresses_controller.rb
+++ app/controllers/public/addresses_controller.rb
@@ -1,18 +1,49 @@
 class Public::AddressesController < ApplicationController
   
   def index
+    @addresses = Address.all
+    @address = Address.new
   end
   
   def edit
   end
   
   def create
+    @address = Address.new(address_params)
+    @address.customer_id = current_customer.id
+    if @address.save
+      redirect_to address_path(@address), notice: "You have created address successfully."
+    else
+      @addresses = Address.all
+      render :index
+    end
   end
   
   def update
+    if @address.update(address_params)
+      redirect_to address_path(@address), notice: "You have updated address successfully."
+    else
+      render "edit"
+    end
   end
   
   def destroy
+    @address.destroy
+    redirect_to addresses_path
   end
+  
+  private
+
+  def address_params
+    params.require(:address).permit(:name, :address, :postal_code)
+  end
+  
+  def ensure_correct_customer
+    @address = Address.find(params[:id])
+    unless @address.customer == current_customer
+      redirect_to addresses_path
+    end
+  end
+  
   
 end
--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -2,12 +2,19 @@ class Public::CustomersController < ApplicationController
   
   
   def show
+    @customer = Customer.find(params[:id])
+    @customer = Customer.new
   end
   
   def edit
   end
   
   def update
+    if @customer.update(customer_params)
+      redirect_to customer_path(@customer), notice: "You have updated customer_info successfully."
+    else
+      render "edit"
+    end
   end
   
   def confirm_withdraw
