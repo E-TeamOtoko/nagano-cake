253219f1c50623e2577d300eaea7428c0187003a
Kudo Rintaro <119265020+KudoRintaro@users.noreply.github.com>
2023-01-23 07:05:18 +0000

    追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,19 @@
 Rails.application.routes.draw do
   devise_for :customers
   devise_for :admins
-  #get "admin/items/new" => "admin/items#new"
-  #post "admin/items" => "admin/items#create"
+  get "admin/items/new" => "admin/items#new"
+  post "admin/items" => "admin/items#create"
 
-  #scope "/admin" do
-    #resources :items, only: [:index, :show, :edit, :update]
-  #end
-  #scope "/public" do
-    #resources :items, only: [:index, :show]
-    #resources :cart_items do
-      #member do
-       #delete "destroy_all"
-      #end
-    #end
-  #end
+  scope "/admin" do
+    resources :items, only: [:index, :show, :edit, :update]
+  end
+  scope "/public" do
+    resources :items, only: [:index, :show]
+    resources :cart_items do
+      member do
+       delete "destroy_all"
+      end
+    end
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
