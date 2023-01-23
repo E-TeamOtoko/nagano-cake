81a30af67327fd09d541af7cf33bf378d42e7334
AaronMorimoto <119596908+AaronMorimoto@users.noreply.github.com>
2023-01-23 14:59:26 +0000

    アクション追加

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -1,15 +1,24 @@
 class Admin::CustomersController < ApplicationController
   
   def index
+    @customers = Customer.all
+    @customer = Customer.new
   end
   
   def show
+    @customer = Customer.find(params[:id])
+    @customer = Customer.new
   end
   
   def edit
   end
   
   def update
+    if @customer.update(customer_params)
+      redirect_to customer_path(@customer), notice: "You have updated customer_info successfully."
+    else
+      render "edit"
+    end
   end
   
 end
