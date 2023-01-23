264ffcd7359e5df938a1f946c7cc23bcf5c11e28
Kudo Rintaro <119265020+KudoRintaro@users.noreply.github.com>
2023-01-23 07:20:58 +0000

    ルーティング修正

--- config/routes.rb
+++ config/routes.rb
@@ -14,5 +14,18 @@ Rails.application.routes.draw do
        delete "destroy_all"
       end
     end
+
+     devise_for :customers, skip: [:passwords], controllers: {
+    registrations: "public/registrations",
+    sessions: "public/sessions"
+  }
+
+  devise_for :admin, skip: [:registrations, :passwords], controllers: {
+    sessions: "admin/sessions"
+  }
+
+  namespace :admin do
+    resources :genres, only: [:index, :create, :edit, :update, :destroy]
+  end
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
