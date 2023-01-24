058f16e2328092c29fbd996430a14e822936e43a
EC2 Default User <117354218+ayumu3221@users.noreply.github.com>
2023-01-24 14:39:23 +0000

    finish admin

---
+++ app/controllers/public/orders_controller.rb
@@ -0,0 +1,5 @@
+class Public::OrdersController < ApplicationController
+    before_action :authenticate_customer!
+    
+
+end
--- app/models/address.rb
+++ app/models/address.rb
@@ -1,2 +1,5 @@
 class Address < ApplicationRecord
+  def address_display
+    '〒' + postal_code + ' ' + address + ' ' + name
+  end
 end
