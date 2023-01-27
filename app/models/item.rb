e2917810a825264b544efc925011516b66733ccd
Kudo Rintaro <119265020+KudoRintaro@users.noreply.github.com>
2023-01-27 07:37:42 +0000

    修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -4,5 +4,11 @@ class Item < ApplicationRecord
   has_many :customers, through: :cart_items
   has_one_attached :image
 
+  validates :image, presence: true
+  validates :item_name, presence: true
+  validates :genre_id, presence: true
+  validates :item_introduction, presence: true
+  validates :tax_excluded_price, presence: true
+
   enum sales_status_method: {sale: true, stop_sale: false}
 end
