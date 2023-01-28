fe6e71391959a622ce9bb03e81cbb40feb999240
Kudo Rintaro <119265020+KudoRintaro@users.noreply.github.com>
2023-01-28 11:36:00 +0000

    修正

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,10 +1,10 @@
 class Order < ApplicationRecord
-    
+
   belongs_to :customer
   has_many :order_items, dependent: :destroy
-  
-  enum payment: { credit_card: 0, transfer: 1 }
+
+  enum payment_method: { credit_card: 0, transfer: 1 }
   enum status: {入金待ち:0,入金確認:1,製作中:2,発送準備中:3,発送済み:4}
-  
-    
+
+
 end
