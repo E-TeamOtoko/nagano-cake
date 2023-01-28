8f0139df414089d3e087a1e1c023a6f6518efbd2
EC2 Default User <117354218+ayumu3221@users.noreply.github.com>
2023-01-28 05:31:52 +0000

    aaz

--- db/migrate/20230118062532_create_items.rb
+++ db/migrate/20230118062532_create_items.rb
@@ -3,7 +3,7 @@ class CreateItems < ActiveRecord::Migration[6.1]
     create_table :items do |t|
       t.references :genre, null: false, foreign_key: true
       t.string :item_name, null: false
-      t.text :item_introduction
+      t.text :item_introduction, null: false
       t.integer :tax_excluded_price, null: false, index: true
       t.boolean :sales_status, null: false, default: true
       t.timestamps
